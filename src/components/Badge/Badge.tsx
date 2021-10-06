bdec780b5c86275de45434f714b420b796b8c54e
Ítalo Teixeira <italo.brtx@gmail.com>
2021-10-06 23:14:44 +0200

    test chromatic in CI

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -21,7 +21,7 @@ type BadgeProps = {
 
 export const Badge = ({ text, className }: BadgeProps) => (
   <Container className={className}>
-    <Body type="span" size="XS">
+    <Body type="span" size="S">
       {text}
     </Body>
   </Container>
